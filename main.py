c1c2b628f792651485a061afc54422ff216d3f07
Souadalimess <99185694+Souadalimess@users.noreply.github.com>
2022-06-27 14:08:28 +0200

    test creating db as list

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+from typing import List
+from uuid import uuid4
 from fastapi import FastAPI
+
+from models import Hero
 
 app = FastAPI()
 
+db: List[Hero] = [
+    Hero(
+        id=uuid4(), name="Souad"
+    )
+]
+
 
 @app.get("/")
-def root():
+async def root():
     return {"Init": "First API with FastAPI!"}
+
+
+@app.get("/api/v1/heroes")
+async def fetch_heroes():
+    return db
